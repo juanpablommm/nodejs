44e58d4305a35b153c0644ef6aab3190a9d9fe41
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2022-06-15 11:34:02 -0500

    :wrench: clase-9: analizando como pasar informacion del servidor o controller a una pantilla pug

--- app-with-express/routes/informacion.js
+++ app-with-express/routes/informacion.js
@@ -2,30 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET informacion listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:nombre/:edad', function(req, res, next) {
+  res.render('informacion', {
+    usuario:{
+      nombre:req.params.nombre,
+      edad:req.params.edad
+    }
+  });/*con el metoodo render, mencionamos que vamos
+  a responder renderizando una vista, en este caso la informacion.pug que tenemos
+  en representacion, no hace falta colocar la extension pues el sixtema automaticamente 
+  sabe a cual vista nos referimos.
+  ahora siii queremos psarle informacion, parametros a esa vista lo hcaemos
+  pasandole como segundo parametro a el metodo render un object JSON, en el cual
+  podemos pasar otros objectos dentro de el si se requiere.
+  en este caso estamos asignado a los atributos de nustro objeto usuario
+  los valores de los parametro que vienen por la url*/
 });
 
-/* GET informacion listing. */
-router.post('/', function(req, res, next) {
-  console.log("llegue por pos");
-    res.send({ persona: {
-      nombre:"juancho"
-    } });
-  });  
 
-/**podemos hacer dinamica la url mediante el paso de paramentros enconces para capturar 
- * los parametros que vengan por la url tendriamos que hacer lo siegiente:
- * 
- * 1. al primer parametro de nuesto metodo get verbo, indicarle que viene un parametro de esa
- * url para que lo escuche
- * 2. mediante la cabecera del reques recuperamos el parameteo con ayuda del metodo
- * param y le pasamos el nombre del parameto como lo pusimos a eschucar en el verbo get en
- * este caso
- */
-/* GET informacion listing. */
-router.get('/:paramURL', function(req, res, next) {
-  res.send('el parametro que ha llegado es: ' + req.params.paramURL);
+router.get("/", function(req, res, next){
+  res.render("informacion");
 });
-
 module.exports = router;
