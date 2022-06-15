0637ce3852b34751a5503a9e135ae4df929aefb2
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2022-06-15 13:57:31 -0500

    analizando como manejar un error en la lectura de un archivo para saber como responder en caso de que esto ocurrar

--- app-with-express/routes/lecturaArchivos.js
+++ app-with-express/routes/lecturaArchivos.js
@@ -1,31 +1,30 @@
-/**para trabajr con la lectura de archivos utilizamos le modulo fs (sileSystem)
- * que ya viene en el core de node js
- */
-
 var fs = require('fs');
 
-/*const archivo = fs.readFileSync('./prueba.mp4', "utf-8");*/
-
-const archivo = fs.readFile('./archivoPrueba.txt', "utf-8", function(erro, datos){
-    /*esta funcion como tercer parametro es la que va ha relizar el proceso de collback, es 
-    lo que se va a ejecutar despues de que leamos el archivo,
-    basicamente lo que vamos hacer es que despues de que leamos el archivo vamos a obtner informacion
-    sobre el.
-    esta funcion recibe como parametros dos tados, el peimero seria el errror, en caso de que
-    tengamos un erro al intentar leer el archivo, que puede ir de la ruta o en caso de que los datos
-    esren corrompidos
-    y el sgundo parametro son los datos*/
 
 
-    console.log("esta es lo que contien el texto ", datos);
+const archivo = fs.readFile('./archivoPrueba.txtm', "utf-8", (error, datos)=>{
+    
+    /**sabiendo que pueden ocurrir algunos error en la lectura de unarchivo,
+     * por ende nuestra funcion recibe dos parametros, uno para identificar el error
+     * y el otro para identificar los datos que fueron leidos.
+     * 
+     * podemos aplicar una logica simple de un if-else para saber como actuar en el caso
+     * de que alla un erro en la lectura del archivo;
+     * por ejemplo vamos a evaluar en al condional si el parametro error esta presente, significaria
+     * que occurrio un error por lo tanto aplicamos nuestra logica para saber como actuar al respecto,
+     * en este caso vamos a imprirmir un mensaje diciendo que hubo un error, pero adicional a eso
+     * vamos a lanzar ese error para que lo puedan ver y saber que paso, al igual que en java
+     * para el alnzamiento de una exception de este lado nosotros tambi utilizaremos la plabra
+     * reservada throw para lanzar el error, con la difernecia que no tenemos excepcione en js
+     */
+    if (error) {
+        console.log("error en la lectura del archivo...");
+        throw error;/**tambien podemos mostrar el stack del error, o el menssage, etc*/
+    } else {
+        /**si no hubo ningun error enctonces procedmos a aplicar nusra logica */
+        console.log(datos);
+    }
 
 });
-/*la forma correcta de leer un archivo dentro de node js, es mediante el concepto de
-un collback, mdeiante un proceso que no va a tener el proceso que no va atener la ejecucion
-del codigo node*/
 
-
-/**al ser una lectura asincronica primero se ejecuta este bloque de codigo mientras la 
- * lectura del archivo se ejcuta
- */
 console.log("esto es loq eu seguiria despues de leer el archivo " + archivo);
