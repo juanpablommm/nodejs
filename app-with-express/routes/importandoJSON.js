99e3067eec4ec28b229e6766675bf28ff868d76f
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2022-06-16 21:39:05 -0500

    analizando la importacion de un archivo json para trabajarlo

---
+++ app-with-express/routes/importandoJSON.js
@@ -0,0 +1,7 @@
+var libro = require('./libro.json');
+
+/**para importar un json estatico, osea que esta en un archivo,
+ * lo realizamos de la misma manera como si importaramos un modulo, solo
+ * que agreagamos la estenxion
+ */
+console.log(libro);
